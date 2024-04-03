3ddb069ca0379f35a8267267f4cad92abda6fb65
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-04-03 19:06:35 +0200

    create en terraform ok

---
+++ terraform/create-cluster.tf
@@ -0,0 +1,65 @@
+resource "openstack_compute_keypair_v2" "test_keypair" {
+  provider   = openstack.ovh             # Nom du fournisseur déclaré dans provider.tf
+  name       = "ssh-port493"            # Nom de la clé SSH à utiliser pour la création
+  public_key = file("~/.ssh/id_ed25519_DO.pub") # Chemin vers votre clé SSH précédemment générée
+}
+
+resource "openstack_compute_instance_v2" "master" {
+  name        = "Port493-Master1"    # Nom de l'instance
+  provider    = openstack.ovh           # Nom du fournisseur
+  image_name  = "Debian 12"             # Nom de l'image
+  flavor_name = "b3-16"                  # Nom du type d'instance
+  # Nom de la ressource openstack_compute_keypair_v2 nommée test_keypair
+  key_pair    = openstack_compute_keypair_v2.test_keypair.name
+  network {
+    name = "Ext-Net" # Ajoute le composant réseau pour atteindre votre instance
+  }
+}
+
+resource "openstack_compute_instance_v2" "master2" {
+  name        = "Port493-Master2"    # Nom de l'instance
+  provider    = openstack.ovh           # Nom du fournisseur
+  image_name  = "Debian 12"             # Nom de l'image
+  flavor_name = "b3-16"                  # Nom du type d'instance
+  # Nom de la ressource openstack_compute_keypair_v2 nommée test_keypair
+  key_pair    = openstack_compute_keypair_v2.test_keypair.name
+  network {
+    name = "Ext-Net" # Ajoute le composant réseau pour atteindre votre instance
+  }
+}
+
+resource "openstack_compute_instance_v2" "node1" {
+  name        = "Port493-Node1"    # Nom de l'instance
+  provider    = openstack.ovh           # Nom du fournisseur
+  image_name  = "Debian 12"             # Nom de l'image
+  flavor_name = "b3-16"                  # Nom du type d'instance
+  # Nom de la ressource openstack_compute_keypair_v2 nommée test_keypair
+  key_pair    = openstack_compute_keypair_v2.test_keypair.name
+  network {
+    name = "Ext-Net" # Ajoute le composant réseau pour atteindre votre instance
+  }
+}
+
+resource "openstack_compute_instance_v2" "node2" {
+  name        = "Port493-Node2"    # Nom de l'instance
+  provider    = openstack.ovh           # Nom du fournisseur
+  image_name  = "Debian 12"             # Nom de l'image
+  flavor_name = "b3-16"                  # Nom du type d'instance
+  # Nom de la ressource openstack_compute_keypair_v2 nommée test_keypair
+  key_pair    = openstack_compute_keypair_v2.test_keypair.name
+  network {
+    name = "Ext-Net" # Ajoute le composant réseau pour atteindre votre instance
+  }
+}
+
+resource "openstack_compute_instance_v2" "node3" {
+  name        = "Port493-Node3"    # Nom de l'instance
+  provider    = openstack.ovh           # Nom du fournisseur
+  image_name  = "Debian 12"             # Nom de l'image
+  flavor_name = "b3-16"                  # Nom du type d'instance
+  # Nom de la ressource openstack_compute_keypair_v2 nommée test_keypair
+  key_pair    = openstack_compute_keypair_v2.test_keypair.name
+  network {
+    name = "Ext-Net" # Ajoute le composant réseau pour atteindre votre instance
+  }
+}
